b88ab09ae9a5a40f99db65424e7f58c1eec15c29
Yaksha-RN <yaksha.nandan@bharatagri.com>
2022-06-14 16:38:23 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HMSReactiveStore } from '@100mslive/hms-video-store';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vc-new';
+   hms = new HMSReactiveStore();
+ hmsStore = this.hms.getStore();
+ hmsActions = this.hms.getActions();
+
 }
